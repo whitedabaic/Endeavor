af1c286f3d6b71ebd9f678b94338bbbd5704a188
whitedabaic <1951893824@qq.com>
2025-02-05 19:47:28 +0800

    add VertexArray

--- Endeavor/src/Endeavor/Application.h
+++ Endeavor/src/Endeavor/Application.h
@@ -11,6 +11,7 @@
 
 #include "Endeavor/Renderer/Shader.h"
 #include "Endeavor/Renderer/Buffer.h"
+#include "Endeavor/Renderer/VertexArray.h"
 
 namespace Endeavor {
 	class ENDEAVOR_API Application
@@ -36,10 +37,12 @@ namespace Endeavor {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray;
-		std::unique_ptr<Shader> m_Shader;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
+
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
+
 	private:
 		static Application* s_Instance;
 	};
---
+++ Endeavor/src/Endeavor/Renderer/VertexArray.cpp
@@ -0,0 +1,20 @@
+#include "edpch.h"
+#include "VertexArray.h"
+
+#include "Renderer.h"
+#include "Platform/OpenGL/OpenGLVertexArray.h"
+
+namespace Endeavor {
+
+	VertexArray* VertexArray::Create()
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::None:			ED_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::OpenGL:		return new OpenGLVertexArray();
+		}
+
+		ED_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+}
