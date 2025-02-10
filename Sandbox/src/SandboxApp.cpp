5d342d706d43f91fc764811b1a8f47be1cc26473
whitedabaic <1951893824@qq.com>
2025-02-10 15:17:28 +0800

    Material System

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -109,20 +109,22 @@ public:
 			}
 		)";
 
-		std::string blueShaderFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 			
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
 
+			uniform vec4 u_Color;
+
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = u_Color;
 			}
 		)";
 
-		m_BlueShader.reset(new Endeavor::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
+		m_FlatColorShader.reset(new Endeavor::Shader(blueShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 	void OnUpdate(Endeavor::Timestep ts) override
 	{
@@ -150,18 +152,29 @@ public:
 		Endeavor::Renderer::BeginScene(m_Camera);
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
+		glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+		//Endeavor::MaterialRef material = new Endeavor::Material(m_FlatColorShader);
+		//material->Set("u_Color", redColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Endeavor::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				if (x % 2 == 0)
+					m_FlatColorShader->UploadUniformFloat4("u_Color", redColor);
+				else
+					m_FlatColorShader->UploadUniformFloat4("u_Color", blueColor);
+				Endeavor::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 
 			}
 		}
 
-		//Endeavor::Renderer::Submit(m_Shader, m_VertexArray);
+		Endeavor::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Endeavor::Renderer::EndScene();
 
@@ -182,7 +195,7 @@ private:
 	std::shared_ptr<Endeavor::Shader> m_Shader;
 	std::shared_ptr<Endeavor::VertexArray> m_VertexArray;
 
-	std::shared_ptr<Endeavor::Shader> m_BlueShader;
+	std::shared_ptr<Endeavor::Shader> m_FlatColorShader;
 	std::shared_ptr<Endeavor::VertexArray> m_SquareVA;
 
 	Endeavor::OrthographicCamera m_Camera;
