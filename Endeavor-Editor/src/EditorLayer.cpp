37fe05ec975350690a59eb068705869ebe0c386d
whitedabaic <1951893824@qq.com>
2025-02-23 21:13:12 +0800

    functions to be virtual

--- Endeavor-Editor/src/EditorLayer.cpp
+++ Endeavor-Editor/src/EditorLayer.cpp
@@ -41,6 +41,8 @@ namespace Endeavor {
 		public:
 			void OnCreate()
 			{
+				auto& transform = GetComponent<TransformComponent>().Transform;
+				transform[3][0] = rand() % 10 - 5.0f;
 			}
 
 			void OnDestroy()
@@ -64,7 +66,7 @@ namespace Endeavor {
 		};
 
 		m_CameraEntity.AddComponent<NativeScriptComponent>().Bind<CameraController>();
-
+		m_SecondCamera.AddComponent<NativeScriptComponent>().Bind<CameraController>();
 	}
 
 	void EditorLayer::OnDetach()
