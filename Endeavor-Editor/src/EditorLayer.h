7f9b9b091ef30226cd8aa831c2b6c238d859d92e
whitedabaic <1951893824@qq.com>
2025-03-01 22:44:04 +0800

    Added play/stop button to editor

--- Endeavor-Editor/src/EditorLayer.h
+++ Endeavor-Editor/src/EditorLayer.h
@@ -28,6 +28,12 @@ namespace Endeavor {
 		void OpenScene();
 		void OpenScene(const std::filesystem::path& path);
 		void SaveSceneAs();
+
+		void OnScenePlay();
+		void OnSceneStop();
+
+		// UI Panels
+		void UI_Toolbar();
 	private:
 		OrthographicCameraController m_CameraController;
 
@@ -57,8 +63,17 @@ namespace Endeavor {
 
 		int m_GizmoType = -1;
 
+		enum class SceneState
+		{
+			Edit = 0, Play = 1
+		};
+		SceneState m_SceneState = SceneState::Edit;
+
 		// Panels
 		SceneHierarchyPanel m_SceneHierarchyPanel;
 		ContentBrowserPanel m_ContentBrowserPanel;
+
+		// Edit resources
+		Ref<Texture2D> m_IconPlay, m_IconStop;
 	};
 }
