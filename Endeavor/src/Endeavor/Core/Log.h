6a4c1f1ed9f950ebed416b88eb2fb81d76469722
whitedabaic <1951893824@qq.com>
2025-03-01 22:51:57 +0800

    Maintenance

--- Endeavor/src/Endeavor/Core/Log.h
+++ Endeavor/src/Endeavor/Core/Log.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/gtx/string_cast.hpp"
+
 #include "Endeavor/Core/Base.h"
 
 #pragma warning(push, 0)
@@ -19,6 +22,24 @@ namespace Endeavor {
 		static std::shared_ptr<spdlog::logger> s_CoreLogger;
 		static std::shared_ptr<spdlog::logger> s_ClientLogger;
 	};
+}
+
+template<typename OStream, glm::length_t L, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::vec<L, T, Q>& vector)
+{
+	return os << glm::to_string(vector);
+}
+
+template<typename OStream, glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::mat<C, R, T, Q>& matrix)
+{
+	return os << glm::to_string(matrix);
+}
+
+template<typename OStream, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, glm::qua<T, Q> quaternio)
+{
+	return os << glm::to_string(quaternio);
 }
 
 // Core log macros
